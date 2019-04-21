7cee587d2699382c8dfdeec84f6e9c6363e34f7e
4poki4 <32469638+4poki4@users.noreply.github.com>
2019-04-21 08:01:11 +0300

    Update finish-installation-script.sh

--- finish-installation-script.sh
+++ finish-installation-script.sh
@@ -6,8 +6,11 @@ wget https://s3.amazonaws.com/koken-installer/releases/Koken_Installer.zip -O Ko
 unzip Koken_Installer.zip -d html/ && rm Koken_Installer.zip || exit 1
 mv html/koken/index.php html/koken/index1.php && cat html/koken/index1.php | sed "s/loopback\['fail'\]/loopback\['true'\]/g" > html/koken/index.php && rm html/koken/index1.php  || exit 1
 chmod -R 777 html/koken || exit 1
-apt-get update && apt install apache2-utils -y && htpasswd koken/config/.htpasswd $1 || exit 1
+apt-get update && apt install apache2-utils pwgen -y && htpasswd koken/config/.htpasswd $1 || exit 1
+MYSQL_ROOT_PASSWORD=`pwgen -1`
+MYSQL_DATABASE=koken_db_`pwgen -1`
+MYSQL_USER=koken_db_user_`pwgen -1`
+MYSQL_PASSWORD=`pwgen -1`
 docker-compose up -d --build || exit 1
-echo 'All done!'
-bash -c 'sleep 1 && rm finish-installation-script.sh' &
+bash -c 'sleep 1 && rm finish-installation-script.sh || exit 1' &
 
