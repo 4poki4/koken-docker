ceb22725c337ce365b7e0cd8f1dc9dea5b193a11
4poki4 <32469638+4poki4@users.noreply.github.com>
2019-04-21 08:08:47 +0300

    Update finish-installation.sh

--- finish-installation.sh
+++ finish-installation.sh
@@ -7,10 +7,10 @@ unzip Koken_Installer.zip -d html/ && rm Koken_Installer.zip || exit 1
 mv html/koken/index.php html/koken/index1.php && cat html/koken/index1.php | sed "s/loopback\['fail'\]/loopback\['true'\]/g" > html/koken/index.php && rm html/koken/index1.php  || exit 1
 chmod -R 777 html/koken || exit 1
 apt-get update && apt install apache2-utils pwgen -y && htpasswd koken/config/.htpasswd $1 || exit 1
-MYSQL_ROOT_PASSWORD=`pwgen -1`
-MYSQL_DATABASE=koken_db_`pwgen -1`
-MYSQL_USER=koken_db_user_`pwgen -1`
-MYSQL_PASSWORD=`pwgen -1`
+DB_ROOT_PASSWORD=`pwgen -1`
+DB_DATABASE=koken_db_`pwgen -1`
+DB_USER=koken_db_user_`pwgen -1`
+DB_USER_PASSWORD=`pwgen -1`
 docker-compose up -d --build || exit 1
 bash -c 'sleep 1 && rm finish-installation.sh || exit 1' &
 
