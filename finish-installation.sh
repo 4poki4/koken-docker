0dea9d4e2623152c559378e8c90e07c89d8f32f7
4poki4 <32469638+4poki4@users.noreply.github.com>
2020-04-18 01:45:24 +0300

    Update finish-installation.sh

--- finish-installation.sh
+++ finish-installation.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-mkdir -p html/koken logs dbdata || exit 1
+mkdir -p html logs dbdata || exit 1
 touch logs/access.log && chmod 666 logs/access.log || exit 1
 wget https://s3.amazonaws.com/koken-installer/releases/Koken_Installer.zip -O Koken_Installer.zip || exit 1
 unzip Koken_Installer.zip -d html/ && rm Koken_Installer.zip || exit 1
+mv koken html/ || exit 1
 mv html/koken/index.php html/koken/index1.php && cat html/koken/index1.php | sed "s/loopback\['fail'\]/loopback\['true'\]/g" > html/index.php && rm -rf html/koken || exit 1
 chmod -R 777 html/koken || exit 1
 apt-get update && apt install apache2-utils pwgen -y && htpasswd koken/config/.htpasswd $1 || exit 1
