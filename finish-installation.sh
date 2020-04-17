fe5ded7d086a0cf57f5997d71b2b31c07079a028
4poki4 <32469638+4poki4@users.noreply.github.com>
2020-04-18 01:41:55 +0300

    Update finish-installation.sh

--- finish-installation.sh
+++ finish-installation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir html logs dbdata || exit 1
+mkdir html/koken logs dbdata || exit 1
 touch logs/access.log && chmod 666 logs/access.log || exit 1
 wget https://s3.amazonaws.com/koken-installer/releases/Koken_Installer.zip -O Koken_Installer.zip || exit 1
 unzip Koken_Installer.zip -d html/ && rm Koken_Installer.zip || exit 1
